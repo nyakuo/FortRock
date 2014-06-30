02ecdf47b73733154886f16d3e3dca9f8509e8cd
yamashita <yamahita@yamashita_nj_lab>
2014-06-30 14:49:49 +0900

    numの削除 fortrock.cpp/grub_variables()においてLabel名のサニタイジング処理を行うため

--- label.h
+++ label.h
@@ -5,26 +5,22 @@
 class Label {
   std::string name;
   std::string asm_name;
-  unsigned num;
 
 public:
-  Label() : name("undefined"), num(0),
+ Label() : name("undefined"),
             asm_name("asm_undefined") {}
 
   // setter
   void set_name(const std::string name)         { this->name = name; }
   void set_asm_name(const std::string asm_name) { this->asm_name = asm_name; }
-  void set_num(const unsigned num)              { this->num = num; }
 
   // getter
   std::string get_name(void)     { return name; }
   std::string get_asm_name(void) { return asm_name; }
-  unsigned get_num(void)         { return num; }
 
   std::string to_string(void) {
     std::string ret_str = "name: " + name;
-    ret_str += " num: " + std::to_string(num);
-
+    ret_str += " asm_name: " + asm_name;
     return ret_str;
   }
 
