94eeefae1175aff67e10665fa2b1ef15d22bcaf1
nyakuo <ynyakuo@gmail.com>
2015-05-14 23:12:21 +0900

    CDFG_Connect.*の削除 ← 不必要となったため

--- cpp_test_code/verilog_generator/CDFG_Connect.cpp
+++
@@ -1,6 +0,0 @@
-#include "CDFG_Connect.h"
-
-CDFG_Connect::CDFG_Connect(const unsigned & exec_state,
-                           const CDFG_Element * connect_to)
-  : exec_state(exec_state),
-    connect_to(connect_to) {}
