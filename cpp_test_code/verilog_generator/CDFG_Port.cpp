bbbb2cdd050ffdd477522593b34e58a23e48af7f
nyakuo <ynyakuo@gmail.com>
2015-06-02 14:13:48 +0900

    接続されているノードのゲッタの追加

--- cpp_test_code/verilog_generator/CDFG_Port.cpp
+++ cpp_test_code/verilog_generator/CDFG_Port.cpp
@@ -1,7 +1,60 @@
 #include "CDFG_Port.hpp"
 
-CDFG_Port::CDFG_Port(const unsigned & bit_width,
-                     const bool & is_signed)
-  : _bit_width(bit_width),
-    _is_signed(is_signed)
+CDFG_Port::CDFG_Port(const std::string & name,
+                     const unsigned & bit_width,
+                     const bool & is_signed,
+                     const eType & type)
+  : _name(name),
+    _bit_width(bit_width),
+    _is_signed(is_signed),
+    _type(type)
 {}
+
+CDFG_Port::~CDFG_Port() {}
+
+
+/**
+   ポートのビット幅の取得
+   @return ポートのビット幅
+ */
+unsigned
+CDFG_Port::get_bit_width(void) const{
+  return this->_bit_width;
+}
+
+/**
+   ポートがsignedかどうかを取得
+   @return ポートがsignedかどうか
+ */
+bool
+CDFG_Port::get_is_signed(void) const {
+  return this->_is_signed;
+}
+
+/**
+   ポートの種類を取得
+   @return 種類
+ */
+CDFG_Port::eType
+CDFG_Port::get_type(void) const {
+  return this->_type;
+}
+
+/**
+   ポートの名前を取得
+   @note ポートの名前と接続されている信号名は同一ではない
+   @return ポートの名前
+ */
+std::string
+CDFG_Port::get_name(void) const {
+  return this->_name;
+}
+
+/**
+   接続されているノードの参照を取得
+   @return 接続されているノードの参照
+ */
+std::shared_ptr<CDFG_Node>
+CDFG_Port::get_node(void) const {
+  return this->_node;
+}
--- cpp_test_code/verilog_generator/CDFG_Port.hpp
+++ cpp_test_code/verilog_generator/CDFG_Port.hpp
@@ -1,6 +1,10 @@
 #ifndef _CDFG_PORT_H
 #define _CDFG_PORT_H
 
+#include <string>
+#include <memory>
+#include "CDFG_Node.hpp"
+
 /**
    @class CDFG_Port
    @brief 演算器のポートの定義を行うクラス
@@ -10,15 +14,38 @@
 */
 class CDFG_Port {
 public:
-  CDFG_Port(const unsigned & bit_width,
-            const bool & is_signed);
+  /** 入出力のポートの種類 */
+  enum class eType : unsigned {
+    CLK = 0,                    // clock
+      CE,                       // clock enable
+      IN,                       // input
+      OUT,                      // output
+      OTH                       // other
+  };
 
-  inline unsigned get_bit_width(void) const { return this->_bit_width; }
-  inline bool get_is_signed(void) const { return this->_is_signed; }
+  CDFG_Port(const std::string & name,
+            const unsigned & bit_width,
+            const bool & is_signed,
+            const eType & type);
+  CDFG_Port(const std::string & name,
+            std::shared_ptr<CDFG_Node> & node)
+    : _name(name),
+      _node(node) {} // test code
+  ~CDFG_Port();
+
+  /* ゲッター */
+  unsigned get_bit_width(void) const;
+  bool get_is_signed(void) const;
+  eType get_type(void) const;
+  std::string get_name(void) const;
+  std::shared_ptr<CDFG_Node> get_node(void) const;
 
 private:
-  unsigned _bit_width;
-  bool _is_signed;
+  unsigned _bit_width;              /** 入出力ポートのビット幅 */
+  bool _is_signed;                  /** 入出力ポートの扱い */
+  eType _type;                      /** 入出力ポートの種類 */
+  std::string _name;                /** 入出力ポートの名前 */
+  std::shared_ptr<CDFG_Node> _node; /** ポートに接続される入出力ポート */
 };
 
 #endif
