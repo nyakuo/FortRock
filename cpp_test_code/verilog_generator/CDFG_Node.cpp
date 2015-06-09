4bb2be633be1436d8ff8955fb1c501fc9f162d2f
yamashita <ynyakuo@gmail.com>
2015-06-09 15:40:01 +0900

    enum eNodeをビット演算に対応させた

--- cpp_test_code/verilog_generator/CDFG_Node.cpp
+++ cpp_test_code/verilog_generator/CDFG_Node.cpp
@@ -1,11 +1,26 @@
-#include "CDFG_Node.h"
+#include "CDFG_Node.hpp"
 
 CDFG_Node::CDFG_Node(const std::string & name,
                      const unsigned & bit_width,
                      const bool & is_signed,
-                     const CDFG_Node::eNode & type)
+                     const CDFG_Node::eNode & type,
+                     const double & parameter,
+                     const long & address,
+                     const long & access_port)
   : _name(name),
     _bit_width(bit_width),
     _is_signed(is_signed),
-    _type(type)
+    _type(type),
+    _parameter(parameter),
+    _address(address),
+  _access_port(access_port)
 {}
+
+/* ゲッター */
+std::string CDFG_Node::get_name(void) const { return this->_name; }
+unsigned CDFG_Node::get_bit_width(void) const { return this->_bit_width; }
+bool CDFG_Node::get_is_signed(void) const { return this->_is_signed; }
+CDFG_Node::eNode CDFG_Node::get_type(void) const { return this->_type; }
+double CDFG_Node::get_parameter(void) const { return this->_parameter; }
+long CDFG_Node::get_address(void) const { return this->_address; }
+long CDFG_Node::get_access_port(void) const { return this->_access_port; }
--- cpp_test_code/verilog_generator/CDFG_Node.hpp
+++ cpp_test_code/verilog_generator/CDFG_Node.hpp
@@ -3,38 +3,54 @@
 
 #include <string>
 
+/**
+   @class CDFG_Node
+   @brief 演算器やモジュール(関数)を表すクラス
+ */
 class CDFG_Node {
 public:
   enum class eNode : unsigned {
-    REG = 0,
-      WIRE,
-      IN,
-      OUT,
-      PARAM,
-      ADD,
-      MINUS,
-      MUL,
-      DIV,
-      FUNC
+      REG   = 1 << 0,
+      WIRE  = 1 << 1,
+      IN    = 1 << 2,
+      OUT   = 1 << 3,
+      PARAM = 1 << 4,
+      ARRAY = 1 << 5,
+      CLK   = 1 << 6,
+      RES   = 1 << 7,
+      REQ   = 1 << 8,
+      CE    = 1 << 9,
+      FIN   = 1 << 10,
+      STATE = 1 << 11,
+      STEP  = 1 << 12,
       };
 
   CDFG_Node(const std::string & name,
             const unsigned & bit_width,
             const bool & is_signed,
-            const CDFG_Node::eNode & type);
+            const CDFG_Node::eNode & type,
+            const double & parameter = 0,
+            const long & address = 0,
+            const long & access_port = 0);
+  ~CDFG_Node(){}
 
-  inline CDFG_Node::eNode get_type(void) const { return this->_type; }
-  inline std::string get_name(void) const { return this->_name; }
-  inline unsigned get_bit_width(void) const { return this->_bit_width; }
-  inline bool get_is_signed(void) const { return this->_is_signed; }
+  /* ゲッター */
+  std::string get_name(void) const;
+  unsigned get_bit_width(void) const;
+  bool get_is_signed(void) const;
+  CDFG_Node::eNode get_type(void) const;
+  double get_parameter(void) const;
+  long get_address(void) const;
+  long get_access_port(void) const;
 
 private:
-  CDFG_Node::eNode _type;
-  std::string _name;
-  bool _is_signed;
-  unsigned _bit_width;
-  // todo: parameter用定数が足りない
-  // todo: 演算器用入出力ポートの定義ができない
+  std::string _name;      /** ノードの名前 */
+  unsigned _bit_width;    /** ビット幅 */
+  bool _is_signed;        /** signedかどうか */
+  CDFG_Node::eNode _type; /** ノードの種類 */
+  double _parameter;      /** parameter用定数 */
+  long _address;          /** アドレス (only array) */
+  long _access_port;       /** アクセスポート (only array) */
 };
 
 #endif
