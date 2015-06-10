b72eb50b8a4b95d91767dac68d3cd702da3ea0fc
yamashita <ynyakuo@gmail.com>
2015-06-10 18:01:35 +0900

    CDFG_Portクラスの仕様の廃止

--- cpp_test_code/verilog_generator/CDFG_Element.cpp
+++ cpp_test_code/verilog_generator/CDFG_Element.cpp
@@ -1,47 +1,118 @@
-#include "CDFG_Element.h"
+#include "CDFG_Element.hpp"
 
-CDFG_Element::CDFG_Element(void) {
-  this->_ope = NULL;
+CDFG_Element::CDFG_Element(std::shared_ptr<CDFG_Operator> & ope) {
+  this->set_operator(ope);
 }
 
-int CDFG_Element::set_ope(std::shared_ptr< CDFG_Node > & ope) {
+/**
+   オペレータのセット
+   @param[in] ope セットするオペレータ
+   @return 1 オペレータを上書き, 0 オペレータを新規にセット
+ */
+int CDFG_Element::set_operator(std::shared_ptr<CDFG_Operator> & ope) {
   int ret = 0;
+
   // overwrite
   if (this->_ope) {
     this->_ope.reset();
     ret = 1;
   }
+  this->_ope = ope;
 
-  this->_ope = std::move(ope);
+  // 入出力数の設定
+  this->_input_list.resize(ope->get_num_input());
+  this->_output_list.resize(ope->get_num_output());
 
   return ret;
 }
 
-void CDFG_Element::set_input_size(const unsigned & size) {
-  this->_input_list.reserve(size);
-}
-
-void CDFG_Element::set_output_size(const unsigned & size) {
-  this->_output_list.reserve(size);
-}
-
-int CDFG_Element::set_input(std::shared_ptr< CDFG_Node > & input, const unsigned & number) {
+/**
+   入力のセット
+   @param[in] input セットする入力ノード
+   @param[in] number セットする位置
+   @return 0 セットする位置が有効, -1 セットする位置が無効
+ */
+int CDFG_Element::set_input(std::shared_ptr<CDFG_Node> & input,
+                            const unsigned & number) {
   if (number > this->_input_list.size())
     return -1;
 
-  this->_input_list[number] = move(input);
+  this->_input_list[number] = input;
   return 0;
 }
 
-int CDFG_Element::set_output(std::shared_ptr< CDFG_Node > & output, const unsigned & number) {
+/**
+   出力のセット
+   @param[in] output セットする出力ノード
+   @param[in] number セットする位置
+   @return 0 セットする位置が有効, -1 セットする位置が無効
+*/
+int CDFG_Element::set_output(std::shared_ptr<CDFG_Node> & output,
+                             const unsigned & number) {
   if (number > this->_output_list.size())
     return -1;
 
-  this->_output_list[number] = move(output);
+  this->_output_list[number] = output;
   return 0;
 }
 
-
+/**
+   実行ステート(CFG)のセット
+   @param[in] state 処理の実行開始ステート (CFG)
+ */
 void CDFG_Element::set_state(const unsigned & state) {
   this->_state = state;
+}
+
+/**
+   実行ステップ(clockベースのタイミング)のセット
+   @param[in] step 処理の実行タイミング
+*/
+void CDFG_Element::set_step(const unsigned & step) {
+  this->_step = step;
+}
+
+/**
+   指定した位置の入力ノードの取得
+   @param[in] at 入力ノードの位置
+   @return 入力ノード
+ */
+std::shared_ptr<CDFG_Node>
+CDFG_Element::get_input_at(const unsigned & at) {
+  return this->_input_list.at(at);
+}
+
+/**
+   指定した位置の出力ノードの取得
+   @param[in] at 出力ノードの位置
+   @return 出力ノード
+ */
+std::shared_ptr<CDFG_Node>
+CDFG_Element::get_output_at(const unsigned & at) {
+  return this->_output_list.at(at);
+}
+
+/**
+   演算に用いる演算器の取得
+   @return 演算に用いる演算器
+ */
+std::shared_ptr<CDFG_Operator>
+CDFG_Element::get_operator(void) {
+  return this->_ope;
+}
+
+/**
+   演算ステート(CFGのベースのタイミング)の取得
+   @return 演算ステート
+ */
+unsigned CDFG_Element::get_state(void) {
+  return this->_state;
+}
+
+/**
+   演算ステップ(DFGベースのタイミング)の取得
+   @return 演算ステップ
+ */
+unsigned CDFG_Element::get_step(void) {
+  return this->_step;
 }
--- cpp_test_code/verilog_generator/CDFG_Element.hpp
+++ cpp_test_code/verilog_generator/CDFG_Element.hpp
@@ -4,23 +4,43 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-#include "CDFG_Node.h"
+#include "CDFG_Node.hpp"
+#include "CDFG_Operator.hpp"
 
+/**
+   @class CDFG_Element
+   @brief 入出力の接続を表すクラス
+
+   モジュールや演算器に対して入出力がどのタイミングで
+   与えられるかを表す
+*/
 class CDFG_Element {
-private:
-  std::vector< std::shared_ptr< CDFG_Node > > _input_list;
-  std::vector< std::shared_ptr< CDFG_Node > > _output_list;
-  std::shared_ptr< CDFG_Node > _ope;
-  unsigned _state;
-
 public:
-  CDFG_Element(void);
-  int set_ope(std::shared_ptr< CDFG_Node > & ope);
-  void set_input_size(const unsigned & size);
-  void set_output_size(const unsigned & size);
-  int set_input(std::shared_ptr< CDFG_Node > & input, const unsigned & number);
-  int set_output(std::shared_ptr< CDFG_Node > & input, const unsigned & number);
+  CDFG_Element(std::shared_ptr<CDFG_Operator> & ope);
+  ~CDFG_Element(void) {}
+
+  /* セッター */
+  int set_operator(std::shared_ptr<CDFG_Operator> & ope);
+  int set_input(std::shared_ptr<CDFG_Node> & input,
+                const unsigned & number);
+  int set_output(std::shared_ptr<CDFG_Node> & input,
+                 const unsigned & number);
   void set_state(const unsigned & state);
+  void set_step(const unsigned & step);
+
+  /* ゲッター */
+  std::shared_ptr<CDFG_Node> get_input_at(const unsigned & at);
+  std::shared_ptr<CDFG_Node> get_output_at(const unsigned & at);
+  std::shared_ptr<CDFG_Operator> get_operator(void);
+  unsigned get_state(void);
+  unsigned get_step(void);
+
+private:
+  std::vector<std::shared_ptr<CDFG_Node> > _input_list;  /** 入力リスト */
+  std::vector<std::shared_ptr<CDFG_Node> > _output_list; /** 出力リスト */
+  std::shared_ptr<CDFG_Operator> _ope;                   /** 実行する演算器の参照 */
+  unsigned _state;                                       /** 実行ステート (CFG) */
+  unsigned _step;                                        /** 実行ステップ (clock) */
 };
 
 #endif
