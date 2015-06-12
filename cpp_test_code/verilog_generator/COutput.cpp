3de758d4bbb572c3e516df01c0a43acd8e8334d8
nyakuo <ynyakuo@gmail.com>
2015-06-12 17:54:32 +0900

    インデントを出力しないでファイル出力を行う機能の実装

--- cpp_test_code/verilog_generator/COutput.cpp
+++ cpp_test_code/verilog_generator/COutput.cpp
@@ -6,7 +6,8 @@
    @param[in] indent_width インデントのスペース文字の数
  */
 COutput::COutput(void)
-  : _INDENT_WIDTH(2)
+  : _INDENT_WIDTH(2),
+    _indent_level(0)
 {}
 
 /**
@@ -23,6 +24,10 @@ COutput::~COutput(void) {
  */
 void COutput::open_ofstream(const std::string filename) {
   this->_ofs.open(filename, std::ios::out);
+}
+
+void COutput::close_ofstream(void) {
+  this->_ofs.close();
 }
 
 /**
@@ -46,7 +51,7 @@ std::string COutput::output_indent(void) {
          this->_indent_levelが0以下になる場合は0となる
  */
 void COutput::indent_left(const unsigned & num_level) {
-  if (this->_indent_level - num_level > 0)
+  if ((this->_indent_level - num_level) > 0)
     this->_indent_level -= num_level;
   else
     this->_indent_level = 0;
@@ -62,11 +67,12 @@ void COutput::indent_right(const unsigned & num_level) {
 }
 
 /**
-   ファイルに出力する
+   ファイルに文字列を出力する
    @param[in] str ファイルに出力する文字列
  */
 std::ostream & COutput::operator << (const std::string & str) {
-  return this->_ofs << this->output_indent() << str;
+  this->_ofs << this->output_indent() << str;
+  return this->_ofs.flush();
 }
 
 /**
@@ -79,10 +85,55 @@ std::ostream & COutput::operator << (const char * str) {
 }
 
 /**
-   int型をファイルに出力する
-   @param[in] i ファイルに出力する数字
+   char型をファイルに出力する
+   @param[in] i ファイルに出力する文字
    @note operator << (const std::string & str)に処理を渡している
 */
-std::ostream & COutput::operator << (const int & i) {
+std::ostream & COutput::operator << (const char & i) {
+  return (*this) << std::string(1, i);
+}
+
+/**
+   unsigned int型をファイルに出力
+   @param[in] i ファイルに出力する数字
+   @note operator << (const std::string & str)に処理を渡している
+ */
+std::ostream & COutput::operator << (const unsigned int & i) {
   return (*this) << std::to_string(i);
+}
+
+/**
+   文字列をインデントなしでファイルに出力する
+   @param[in] str ファイルに出力する文字列
+ */
+std::ostream & COutput::operator <<= (const std::string & str) {
+  this->_ofs << str;
+  return this->_ofs.flush();
+}
+
+/**
+   char型の文字列をインデントなしでファイルに出力する
+   @param[in] str ファイルに出力する文字列
+   @note operator <<= (const std::string & str)に処理を渡している
+ */
+std::ostream & COutput::operator <<= (const char * str) {
+  return (*this) <<= std::string(str);
+}
+
+/**
+   char型をインデントなしでファイルに出力する
+   @param[in] i ファイルに出力する文字
+   @note operator <<= (const std::string & str)に処理を渡している
+*/
+std::ostream & COutput::operator <<= (const char & i) {
+  return (*this) <<= std::string(1, i);
+}
+
+/**
+   unsigned int型をインデントなしでファイルに出力
+   @param[in] i ファイルに出力する数字
+   @note operator <<= (const std::string & str)に処理を渡している
+ */
+std::ostream & COutput::operator <<= (const unsigned int & i) {
+  return (*this) <<= std::to_string(i);
 }
--- cpp_test_code/verilog_generator/COutput.hpp
+++ cpp_test_code/verilog_generator/COutput.hpp
@@ -14,13 +14,20 @@ public:
   ~COutput(void);
 
   void open_ofstream(const std::string filename);    /** ofstreamを開く */
-  void indent_left(const unsigned & num_level = 0);  /** インデントを浅くする */
-  void indent_right(const unsigned & num_level = 0); /** インデントを深くする */
+  void close_ofstream(void);                         /** ofstreamを閉じる */
+  void indent_left(const unsigned & num_level = 1);  /** インデントを浅くする */
+  void indent_right(const unsigned & num_level = 1); /** インデントを深くする */
   std::string output_indent(void);                   /** 現在のインデントレベルでスペース文字を返す */
 
-  std::ostream & operator << (const std::string & str); /** ファイルに出力する */
-  std::ostream & operator << (const char * str);        /** ファイルに出力する */
-  std::ostream & operator << (const int & i);           /** ファイルに出力する */
+  std::ostream & operator << (const std::string & str);  /** ファイルに出力する */
+  std::ostream & operator << (const char * str);         /** ファイルに出力する */
+  std::ostream & operator << (const char & i);           /** ファイルに出力する */
+  std::ostream & operator << (const unsigned int & i);   /** ファイルに出力する */
+  std::ostream & operator <<= (const std::string & str); /** ファイルに出力する */
+  std::ostream & operator <<= (const char * str);        /** ファイルに出力する */
+  std::ostream & operator <<= (const char & i);          /** ファイルに出力する */
+  std::ostream & operator <<= (const unsigned int & i);  /** ファイルに出力する */
+
 
 private:
   const unsigned _INDENT_WIDTH; /** インデント幅 (コンストラクタで初期化) */
