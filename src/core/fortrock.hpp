b16df7cc93b1ac40892fa26436de2fc6f1d517db
yamashita <ynyakuo@gmail.com>
2015-06-24 18:31:01 +0900

    privateな文字列の命名規則を変更

--- src/core/fortrock.hpp
+++ src/core/fortrock.hpp
@@ -1,25 +1,10 @@
 #ifndef _FORTROCK_H
 #define _FORTROCK_H
-#include "llvm/ADT/SmallPtrSet.h"
-#include "llvm/ADT/StringExtras.h"
-#include "llvm/Config/config.h"
-#include "llvm/IR/CallingConv.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/InlineAsm.h"
-#include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
-#include "llvm/MC/MCAsmInfo.h"
-#include "llvm/MC/MCInstrInfo.h"
-#include "llvm/MC/MCSubtargetInfo.h"
 #include "llvm/Pass.h"
-#include "llvm/PassManager.h"
-#include "llvm/Support/CommandLine.h"
-#include "llvm/Support/ErrorHandling.h"
-#include "llvm/Support/FormattedStream.h"
-#include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/InstIterator.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include <iostream>
 #include <list>
@@ -30,6 +15,8 @@
 #include "../dfg/CModule.hpp"
 #include "../dfg/COutput.hpp"
 #include "../dfg/CModuleGenerator.hpp"
+#include "../xml/CInstancingOperator.hpp"
+
 using namespace llvm;
 
 /**
@@ -51,7 +38,7 @@ enum inst_opcode {
 
 /**
  * @class FortRock
- * FortRockのメインのパスの実装
+ * @brief FortRockのメインのパスの実装
  */
 class FortRock : public ModulePass  {
 public:
@@ -60,7 +47,7 @@ public:
     : ModulePass(ID)
   {}
 
-  bool runOnModule(Module &M);
+  bool runOnModule(Module & M);
 
   virtual const char *getPassName(void) const
   { return "FortRock: Fortran to Verilog backend"; }
@@ -69,10 +56,13 @@ private:
   std::shared_ptr<CModuleGenerator> _module_gen;
 
   //! ステートマシンの遷移前の状態を保持するレジスタの名前
-  const std::string PREV_STATE_NAME = "prev_state";
+  const std::string _PREV_STATE_NAME = "prev_state";
 
   //! ステートマシンの現在の状態を保持するレジスタの名前
-  const std::string CUR_STATE_NAME = "current_state";
+  const std::string _CUR_STATE_NAME = "current_state";
+
+  //! 演算器の設定ファイル名
+  const std::string _OPERATOR_CONFIG_FILENAME = "./config/operator_config.xml";
 
   // 補助関数
   std::string _get_module_name(const Module & M);
@@ -88,4 +78,5 @@ private:
   void _parse_instructions(const Instruction * inst);
   void _add_load_node(const Instruction * inst);
 }; // FortRock
+
 #endif
