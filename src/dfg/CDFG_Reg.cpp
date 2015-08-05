2d4ce9b893f815bbf91b132ec05f3df04d50dc73
yamashita <ynyakuo@gmail.com>
2015-08-05 18:08:11 +0900

    レジスタノードを表すクラスの実装

---
+++ src/dfg/CDFG_Reg.cpp
@@ -0,0 +1,49 @@
+#include "CDFG_Reg.hpp"
+
+/**
+   入力信号かどうかを取得
+   @return 常にfalse
+ */
+bool
+CDFG_Reg::is_input(void) {
+  return false;
+}
+
+/**
+   出力信号かどうかを取得
+   @return FIN, OUTの場合のみ true
+ */
+bool
+CDFG_Reg::is_output(void) {
+  switch (this->_type)
+    {
+    case eRegType::FIN:
+    case eRegType::OUT:
+      return true;
+
+    default:
+      return false;
+    }
+}
+
+/**
+   信号名の接頭辞を取得
+   @return 信号名の接頭辞
+ */
+std::string
+CDFG_Reg::_get_prefix(void) {
+  switch (this->_type) {
+  case eRegType::IN_COPY:
+  case eRegType::REG:
+    return "r_";
+
+  case eRegType::FIN:
+  case eRegType::OUT:
+    return "o_r_";
+
+  case eRegType::STATE:
+  case eRegType::PREV_STATE:
+  case eRegType::STEP:
+    return "r_sys_";
+  }
+}
---
+++ src/dfg/CDFG_Reg.hpp
@@ -0,0 +1,54 @@
+#ifndef _CDFG_REGNODE_H
+#define _CDFG_REGNODE_H
+
+#include <string>
+#include "CDFG_Node.hpp"
+
+/**
+   @class CDFG_Reg
+   @brief Regを表すクラス
+ */
+class CDFG_Reg : public CDFG_Node {
+public:
+  /** 信号線の種類定数 */
+  enum class eRegType : unsigned {
+    REG = 0,
+      IN_COPY,
+      FIN,
+      OUT,
+      STATE,
+      PREV_STATE,
+      STEP,
+      };
+
+  /**
+     コンストラクタ
+     @param[in] asm_name アセンブリ上での信号名
+     @param[in] bit_width 信号線のビット幅
+     @param[in] is_signed 信号線がsignedかどうか
+     @param[in] type 信号線の種類
+   */
+  CDFG_Reg(const std::string & asm_name,
+           const unsigned & bit_width,
+           const bool & is_signed,
+           const eRegType & type)
+    : _type(type),
+      CDFG_Node(asm_name,
+                bit_width,
+                is_signed,
+                CDFG_Node::eNode::REG,
+                this->_get_prefix())
+  {}
+
+  // getter
+  eRegType get_type(void) { return this->_type; }
+  virtual bool is_input(void) override final;
+  virtual bool is_output(void) override final;
+
+private:
+  eRegType _type; //! 信号線の種類
+
+  std::string _get_prefix(void);
+};
+
+#endif
