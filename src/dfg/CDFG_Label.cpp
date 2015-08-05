c321ed5f8cf99340ae5bf096f46c75569c53e4b4
yamashita <ynyakuo@gmail.com>
2015-08-05 18:18:10 +0900

    to_string()の実装

--- src/dfg/CDFG_Label.cpp
+++ src/dfg/CDFG_Label.cpp
@@ -1,1 +1,21 @@
 #include "CDFG_Label.hpp"
+
+/**
+   ラベルの値をverilogの文法で返す
+   @return ラベルの値
+   例) 8'h12ab
+ */
+std::string
+CDFG_Label::to_string(void) {
+  std::string ret ("");
+  char buf[64];
+
+  // ビット幅
+  ret += std::to_string(this->get_bit_width())
+    + "'h";
+
+  snprintf(buf, sizeof(buf), "%x",
+           this->_label_state);
+
+  return ret + buf;
+}
