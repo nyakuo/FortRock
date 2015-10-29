6dd27015903c8ccffe4755cda1e209deba36c18e
yamashita <ynyakuo@gmail.com>
2015-10-29 17:55:58 +0900

    RAM を用いた store 命令を表すために CDFG_StoreElem クラスを実装

---
+++ src/dfg/CDFG_StoreElem.cpp
@@ -0,0 +1,28 @@
+#include "CDFG_StoreElem.hpp"
+
+/**
+   コンストラクタ
+   @param[in] state 命令実行ステート
+   @param[in] step 命令実行ステップ
+   @param[in] latency
+*/
+CDFG_StoreElem::CDFG_StoreElem
+(const unsigned & state,
+ const unsigned & step,
+ const unsigned & latency)
+  : CDFG_Element(CDFG_Operator::eType::STORE,
+                 1, // 入力数
+                 state,
+                 step,
+                 latency)
+{} // CDFG_StoreElem
+
+/**
+   アクセスに使用するポートの設定
+   @param[in] port_num アクセスに使用するポート
+ */
+void
+CDFG_StoreElem::set_use_port
+(const unsigned & port_num) {
+  this->_port_num = port_num;
+} // set_use_port
---
+++ src/dfg/CDFG_StoreElem.hpp
@@ -0,0 +1,19 @@
+#ifndef _CDFG_STOREELEM_H
+#define _CDFG_STOREELEM_H
+
+#include "CDFG_Element.hpp"
+
+class CDFG_StoreElem : public CDFG_Element {
+public:
+  CDFG_StoreElem(const unsigned & state,
+                 const unsigned & step,
+                 const unsigned & latency);
+
+  // setter
+  void set_use_port(const unsigned & port_num);
+
+private:
+  unsigned _port_num; ///< 使用するアクセスポート
+};
+
+#endif
